af650034646c047fb42a465fefa175ccb04054d6
Alasdair Mercer <mercer.alasdair@gmail.com>
2016-09-21 02:08:15 +0100

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2016 Alasdair Mercer, Skelp
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+module.exports = function(grunt) {
+  var commonjs = require('rollup-plugin-commonjs')
+  var semver = require('semver')
+  var uglify = require('rollup-plugin-uglify')
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    clean: {
+      build: [ 'dist/**' ]
+    },
+
+    eslint: {
+      target: [ 'lib/**/*.js', 'test/**/*.js' ]
+    },
+
+    mochaTest: {
+      test: {
+        options: {
+          clearRequireCache: true,
+          reporter: 'spec'
+        },
+        src: [ 'test/**/*.spec.js' ]
+      }
+    },
+
+    rollup: {
+      umdDevelopment: {
+        options: {
+          format: 'umd',
+          moduleId: 'oopsy',
+          moduleName: 'OOPsy',
+          sourceMap: true,
+          sourceMapRelativePaths: true,
+          plugins: function() {
+            return [
+              commonjs()
+            ]
+          }
+        },
+        files: {
+          'dist/oopsy.js': 'lib/oopsy.js'
+        }
+      },
+      umdProduction: {
+        options: {
+          format: 'umd',
+          moduleId: 'oopsy',
+          moduleName: 'OOPsy',
+          sourceMap: true,
+          sourceMapRelativePaths: true,
+          banner: '/*! OOPsy v<%= pkg.version %> | (C) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> | MIT License */',
+          plugins: function() {
+            return [
+              commonjs(),
+              uglify({
+                output: {
+                  comments: function(node, comment) {
+                    return comment.type === 'comment2' && /^\!/.test(comment.value)
+                  }
+                }
+              })
+            ]
+          }
+        },
+        files: {
+          'dist/oopsy.min.js': 'lib/oopsy.js'
+        }
+      }
+    },
+
+    watch: {
+      all: {
+        files: [ 'lib/**/*.js', 'test/**/*.js' ],
+        tasks: [ 'build', 'mochaTest' ]
+      }
+    }
+  })
+
+  require('load-grunt-tasks')(grunt)
+
+  var buildTasks = [ 'clean', 'rollup' ]
+  var testTasks = [ 'mochaTest' ]
+
+  if (semver.satisfies(process.version, '>=4')) {
+    buildTasks.unshift('eslint')
+    testTasks.unshift('eslint')
+  } else {
+    grunt.log.writeln('eslint task is disabled because Node.js version is too low!')
+  }
+
+  grunt.registerTask('default', [ 'build' ])
+  grunt.registerTask('build', buildTasks)
+  grunt.registerTask('test', testTasks)
+}
--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# oopsy
-Simple OOP for ECMAScript 5
+      .oooooo.     .oooooo.   ooooooooo.
+     d8P'  `Y8b   d8P'  `Y8b  `888   `Y88.
+    888      888 888      888  888   .d88'  .oooo.o oooo    ooo
+    888      888 888      888  888ooo88P'  d88(  "8  `88.  .8'
+    888      888 888      888  888         `"Y88b.    `88..8'
+    `88b    d88' `88b    d88'  888         o.  )88b    `888'
+     `Y8bood8P'   `Y8bood8P'  o888o        8""888P'     .8'
+                                                    .o..P'
+                                                    `Y8P'
+
+[OOPsy](https+://github.com/Skelp/oopsy) makes it easier to use the Object-Orientated Programming (OOP) model in
+JavaScript.
+
+[![Build Status](https://img.shields.io/travis/Skelp/oopsy/develop.svg?style=flat-square)](https://travis-ci.org/Skelp/oopsy)
+[![Dev Dependency Status](https://img.shields.io/david/dev/Skelp/oopsy.svg?style=flat-square)](https://david-dm.org/Skelp/oopsy#info=devDependencies)
+[![License](https://img.shields.io/npm/l/oopsy.svg?style=flat-square)](https://github.com/Skelp/oopsy/blob/master/LICENSE.md)
+[![Release](https://img.shields.io/npm/v/oopsy.svg?style=flat-square)](https://www.npmjs.com/package/oopsy)
+
+* [Install](#install)
+* [API](#api)
+* [Bugs](#bugs)
+* [Contributors](#contributors)
+* [License](#license)
+
+## Install
+
+Install using the package manager for your desired environment(s):
+
+``` bash
+$ npm install --save oopsy
+# OR:
+$ bower install --save oopsy
+```
+
+You'll need to have at least [Node.js](https://nodejs.org) installed and you'll only need [Bower](https://bower.io) if
+you want to install that way instead of using `npm`.
+
+If you want to simply download the file to be used in the browser you can find them below:
+
+* [Development Version](https://github.com/Skelp/oopsy/blob/master/dist/oopsy.js)
+* [Production Version](https://github.com/Skelp/oopsy/blob/master/dist/oopsy.min.js)
+
+## API
+
+TODO: Document
+
+## Bugs
+
+If you have any problems with OOPsy or would like to see changes currently in development you can do so
+[here](https://github.com/Skelp/oopsy/issues).
+
+## Contributors
+
+If you want to contribute, you're a legend! Information on how you can do so can be found in
+[CONTRIBUTING.md](https://github.com/Skelp/oopsy/blob/master/CONTRIBUTING.md). We want your suggestions and pull
+requests!
+
+A list of OOPsy contributors can be found in [AUTHORS.md](https://github.com/Skelp/oopsy/blob/master/AUTHORS.md).
+
+## License
+
+See [LICENSE.md](https://github.com/Skelp/oopsy/raw/master/LICENSE.md) for more information on our MIT license.
+
+© 2016 [Skelp](https://skelp.io)
+<img align="right" width="16" height="16" src="https://cdn.rawgit.com/Skelp/skelp-branding/master/assets/logo/base/skelp-logo-16x16.png">
